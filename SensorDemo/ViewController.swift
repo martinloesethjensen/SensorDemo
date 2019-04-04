12ce7263595fb2405689c51c9dcc14b0a35b9a5d
Martin L. Jensen <martinloesethjensen@gmail.com>
2019-04-04 21:50:54 +0200

    Initial Commit

---
+++ SensorDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SensorDemo
+//
+//  Created by Martin Løseth Jensen on 04/04/2019.
+//  Copyright © 2019 Martin Løseth Jensen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
